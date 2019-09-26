0110a6739547caad5636978b39b0e61373056f12
delightful-instructions-4401 <jeremygray@utexas.edu>
2019-09-26 05:42:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// add solution here
+function theBeatlesPlay (musicians,instruments) {
+  var start = []
+  for (var n=0; n<musicians.length; n++) {
+    start[n]=`${musicians[n]} plays ${instruments[n]}`
+    
+  }
+  return start
+}
+
+function johnLennonFacts (facts) {
+  var result = []
+  let i=0
+  while (i<facts.length) {
+    result[i]=`${facts[i]}!!!`
+    i=i+1
+  }
+  return result
+}
